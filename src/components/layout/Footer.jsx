9d8144a0ce15b7bbbd8447b46ab40e19a5882e76
DAL <149305694+Darrlop@users.noreply.github.com>
2024-04-28 12:04:31 +0200

    inicio estrutura cabecera pie

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <p>
+          2024 - <strong>Chollopop, tu web molona de compraventa</strong>.
+        </p>
+      </div>
+      <div>
+        <a href="https://twitter.com/" target="_blank" title="Twitter/X">
+          <span className="footer__icon">
+            <i className="fi fi-brands-twitter"></i>
+          </span>
+          Twitter
+        </a>
+        <span> - </span>
+        <a href="https://www.instagram.com/" target="_blank" title="Instagram">
+          <span className="footer__icon">
+            <i className="fi fi-brands-instagram"></i>
+          </span>
+          Instagram
+        </a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img
+          className="logo"
+          src="https://res.cloudinary.com/dyyhithd8/image/upload/v1710875622/chollopop-transparente_j1s1zu.png"
+          alt="logo Chollopop"
+        />
+      </div>
+      <div className="header__container__tittle">
+        <h1 className="big">Chollopop</h1>
+        <h3>Tu web molona de compra-venta</h3>
+      </div>
+    </header>
+  );
+}
